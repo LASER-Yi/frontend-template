9d865f7b5372a34d2c3c71055bf23f78395832b8
LASER-Yi <liangyi0007@gmail.com>
2022-01-19 21:23:11 +0800

    replace vite with next.js

--- src/App/index.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FunctionComponent } from "react";
-
-const App: FunctionComponent = () => {
-  return <div className=""></div>;
-};
-
-export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from "react";
+
+interface Props {}
+
+const Home: FunctionComponent<Props> = ({}) => {
+  return <h1>This is a Index Page</h1>;
+};
+
+export default Home;
