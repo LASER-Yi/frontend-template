8b62ad300143954988f24962dba3e6c2da2f4947
LASER-Yi <liangyi0007@gmail.com>
2022-01-20 20:38:03 +0800

    Improve configurations

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
+import "@/styles/globals.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./tailwind.css";
 
 ReactDOM.render(
   <React.StrictMode>
